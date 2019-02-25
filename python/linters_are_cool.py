b5f8794fc2aee01f9699a2733f488ba4f219a6e1
zigaLuksic <ziga.luksic@fmf.uni-lj.si>
2019-02-25 14:45:02 +0100

    Add python linter file

---
+++ python/linters_are_cool.py
@@ -0,0 +1,21 @@
+
+def square_and_sum(x,y) :
+    x= x*x#Square x
+    return y + x #Add y and return
+
+import random
+
+def random_list(n):
+    """ Returns a list of length n with random floats. Returns empty list for negative n. """
+    new lst = []
+    for _ in range(n):
+       new_lst += random.randomint()
+    return new_lst
+
+def positive_num_to_string(n):
+    n = str(n)
+    if n <= 0 then:
+        return n
+    else :
+        raise Negative number!
+

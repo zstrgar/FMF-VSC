55edd36ab467e1fe736ab2e4d178958d6e7c83ca
zstrgar <zstrgar@gmail.com>
2019-03-02 16:54:05 +0100

    delavnica

--- python/linters_are_cool.py
+++ python/linters_are_cool.py
@@ -1,21 +1,24 @@
 
-def square_and_sum(x,y) :
-    x= x*x#Square x
-    return y + x #Add y and return
-
 import random
 
+
+def square_and_sum(x, y):
+    x = x*x  # Square x
+    return y + x  # Add y and return
+
+
 def random_list(n):
-    """ Returns a list of length n with random floats. Returns empty list for negative n. """
-    new lst = []
+    """ Returns a list of length n with random floats. Returns empty list for 
+    negative n. """
+    new_lst = []
     for _ in range(n):
-       new_lst += random.randomint()
+        new_lst += random.randint(10, 100)
     return new_lst
+
 
 def positive_num_to_string(n):
     n = str(n)
-    if n <= 0 then:
+    if n <= 0:
         return n
-    else :
-        raise Negative number!
-
+    else:
+        raise "Negative number!"
